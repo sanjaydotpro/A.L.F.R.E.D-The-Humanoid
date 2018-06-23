0c5802151125536421558e164442d94655fce5e8
AyushDhanai1419 <adhanai1419@gmail.com>
2018-06-23 23:25:00 +0530

    AlphaTestCommit-1

---
+++ README.md
@@ -0,0 +1,2 @@
+# The-Humanoid-Gehu-Official
+A joint collaboration of CS, ECE and ME department of Graphic Era Hill University, with a mission of transforming dreams into reality.
---
+++ alfred.py
@@ -0,0 +1,43 @@
+# A.L.F.R.E.D main libraries and modules 
+import speech_recognition as sr 
+import pyttsx3 as px
+
+# A.L.F.R.E.D supporting modules
+
+
+#for text to speech
+engine = px.init()
+engine.setProperty('rate', 125)  # 125 words per minute
+engine.setProperty('volume', 1)
+voices = engine.getProperty('voices')
+
+
+
+rs = sr.Recognizer()
+with sr.Microphone() as source:
+    print("######### CALIBARATING NOISE LEVEL #############")
+    rs.adjust_for_ambient_noise(source,duration=float(2))
+    # Conversation loop
+    while True:
+        print("######### CALIBARATING NOISE LEVEL #############")
+        rs.adjust_for_ambient_noise(source,duration=float(5))
+        print('Speak ALFRED is listening......')
+        #for male or female voice
+        for voice in voices:
+            engine.setProperty('voice', voice.id)
+        engine.say("I am Listening")
+        engine.runAndWait()
+        spoken_text = rs.listen(source)
+        print('Done Recording')
+
+        try:
+            print('Sending the data to the API')
+            text_recieved = rs.recognize_google(spoken_text)
+            print("You have said :",text_recieved.lower())
+            engine.say("You have said ")
+            engine.runAndWait()
+            engine.say(text_recieved)
+            engine.runAndWait()
+
+        except:
+            print('A.L.F.R.E.D could\'t understand what you said')
